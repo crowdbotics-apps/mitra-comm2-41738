5b995ba1820aa63ebdf89636b5f8e80daea99b47
Crowdbotics <team@crowdbotics.com>
2023-06-26 06:45:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mitra-comm2-41738",
+  url: "https://mitra-comm2-41738.botics.co",
+  api: "https://mitra-comm2-41738.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
